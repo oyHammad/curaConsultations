a2010b29085f95a2c7d2fa42f783dc426c170632
osamaHammad <91420627+oyHammad@users.noreply.github.com>
2022-04-11 14:23:07 +0200

    update users

--- manageUsers.spec.ts
+++ manageUsers.spec.ts
@@ -1,22 +1,44 @@
+/// <reference types="Cypress"/>
+
 import { login } from "../support/models/CuraConsultation/Login/Login"
 import { AddUser } from "../support/models/Users/addUser";
 import { Users } from "../support/models/Users/users";
+import { generateRandomstring } from "../support/randam";
+
 
 const user=new Users()
-const addUser=new AddUser
+const addUser=new AddUser()
+const fullName = generateRandomstring("user")
+const alicNameInput=generateRandomstring("alic")
+const userNameInput =generateRandomstring("userName")
+const emailInput=generateRandomstring("email@")
+const userPhoneInput=generateRandomstring("phone")
+
+
 describe('Manage users',()=>{
 before(()=>{
 
-    const userName="doctor01";
-    const Passwpord="Doctor01P@$$"
     cy.visit('/')
-    login(userName,Passwpord)
+    login()
 })
 it('Add and edit new user',()=>{
- 
+
     user.users().click()
     user.usersButton().click()
     user.addUserButton().click()
+addUser.fullNameInput().type(fullName)
+addUser.alicNameinput().type(alicNameInput)
+addUser.userNameInput().type(userNameInput)
+addUser.emailInput().type("emailInpu")
+addUser.userPhoneInput().type(userPhoneInput)
+addUser.passwordInput().type("Ts@123456")
+addUser.confirmpasswprdInput().type("Ts@123456")
+addUser.isActive().click()
+addUser.saveButton().click()
+
+cy.get("#mat-input-10").type(userNameInput)
+cy.get("body > app-root > app-full-layout > div > mat-sidenav-container > mat-sidenav-content > div > app-list-users > div > div > mat-card > mat-card-content > div.d-flex.search-input-style > button").click()
+cy.get('app-list-users.ng-star-inserted').click()
 
 })
 
