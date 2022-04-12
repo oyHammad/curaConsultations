f4b0a8ea2d952b2bd6fa0fe7db0664621bf8e5df
osamaHammad <91420627+oyHammad@users.noreply.github.com>
2022-04-12 08:12:34 +0200

    Delete Login.ts

--- Login.ts
+++
@@ -1,9 +0,0 @@
-
-export function login (userName:string,password:string):void {
-  
-    cy.get("#mat-input-0").type(userName)
-    cy.get("#mat-input-1").type(password)
-    cy.get(".mat-button-wrapper").click()
- 
-}
-
