b270c974888c709c8d24de3f421bc8eac45f55f4
osamaHammad <91420627+oyHammad@users.noreply.github.com>
2022-04-14 10:16:38 +0200

    Update manageUsers.spec.ts

--- cypress/integration/manageUsers.spec.ts
+++ cypress/integration/manageUsers.spec.ts
@@ -1,12 +1,12 @@
 /// <reference types="Cypress"/>
 
-import { SearchComponent } from "../support/componenets/searchComponent";
 import { login } from "../support/models/CuraConsultation/Login/Login"
 import { AddUser } from "../support/models/Users/addUser";
+import { Edituser } from "../support/models/Users/editUser";
 import { Users } from "../support/models/Users/users";
 import { generateRandomstring } from "../support/randam";
 
-
+const edituser = new Edituser()
 const user=new Users()
 const addUser=new AddUser()
 const fullName = generateRandomstring("user")
@@ -14,7 +14,7 @@ const alicNameInput=generateRandomstring("alic")
 const userNameInput =generateRandomstring("userName")
 const emailInput=generateRandomstring("email@.com")
 const userPhoneInput=generateRandomstring("phone")
-const searchComponent=new SearchComponent()
+const editFullName =generateRandomstring("editFullName")
 
 describe('Manage users',()=>{
 before(()=>{
@@ -23,11 +23,10 @@ before(()=>{
     login()
 })
 it('Add and edit new user',()=>{
- 
-    user.users().click()
-    user.usersButton().click()
-    user.addUserButton().click()
 
+user.users().click()
+user.usersButton().click()
+user.addUserButton().click()
 addUser.fullNameInput().type(fullName)
 addUser.alicNameinput().type(alicNameInput)
 addUser.userNameInput().type(userNameInput)
@@ -38,16 +37,19 @@ addUser.confirmpasswprdInput().type("Ts@123456")
 addUser.isActive().click()
 addUser.saveButton().click()
 
-user.searchRaw().type(userNameInput)
-user.searchButton().click()
-
+cy.get("#mat-input-10").type(userNameInput)
+cy.get("body > app-root > app-full-layout > div > mat-sidenav-container > mat-sidenav-content > div > app-list-users > div > div > mat-card > mat-card-content > div.d-flex.search-input-style > button").click()
 cy.get('app-list-users.ng-star-inserted').contains(userNameInput).should('be.visible')
 cy.get('app-list-users.ng-star-inserted').contains(userNameInput).then((elme)=>{
-if (userNameInput==userNameInput){    
+if (userNameInput===userNameInput){    
     cy.get("body > app-root > app-full-layout > div > mat-sidenav-container > mat-sidenav-content > div > app-list-users > div > div > mat-card > mat-card-content > div.example-container.m-t-20 > app-dbs-data-table > div > table > tbody > tr:nth-child(1) > td.mat-cell.cdk-cell.cdk-column-action.mat-column-action.ng-star-inserted > button").click()
 }
 })
 cy.get('.mat-menu-content > :nth-child(1)').click()
+
+edituser.fullNameInput().clear().type(editFullName)
+edituser.userPhoneInput().clear().type("111111")
+edituser.saveButton().click()
 })
 
 })
